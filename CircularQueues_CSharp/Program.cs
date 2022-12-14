5c464c8ce43095737b93c7efbbe6f9f718a3c37b
TA1092 <bagas.saras.ft21@mail.umy.ac.id>
2022-12-14 16:57:41 +0700

    membuat deklarasi variabel dan method Queues

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace CircularQueues_CSharp
 {
-    internal class Program
+    class Queues
     {
-       
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        
     }
 }
