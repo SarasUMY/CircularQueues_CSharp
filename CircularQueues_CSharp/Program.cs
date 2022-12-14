62c8437987f567e04291e1c959a55e072807fde8
TA1092 <bagas.saras.ft21@mail.umy.ac.id>
2022-12-14 17:03:08 +0700

    membuat method insert

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -15,6 +15,26 @@ namespace CircularQueues_CSharp
             FRONT = -1;
             REAR = -1;
         }
-        
+        public void insert(int element)
+        {
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+            queue_array[REAR] = element;
+        }
     }
 }
